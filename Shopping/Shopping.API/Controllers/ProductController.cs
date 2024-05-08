0baf8f5ede2e0bddc79cac270809178c0b4edfa4
Munna Kumar <kumarbadal1434@gmail.com>
2024-05-08 12:57:25 +0530

    Shopping api devloped

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Shopping.API.Data;
+using MongoDB.Driver;
+
+namespace Shopping.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _context;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ProductContext context, ILogger<ProductController> logger)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context
+                            .Products
+                            .Find(p => true)
+                            .ToListAsync();
+        }
+    }
+}
